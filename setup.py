943ff4aff9e913c37667f1e68c9b7bffbc079c4f
Arpit Sinha <arpit.sinha1609@gmail.com>
2024-03-02 18:41:41 +0530

    New commit for setup and requirememts

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 ## setup.py enables you to crate an application as a package
 
 from setuptools import find_packages,setup
-#from typing import List
+from typing import List
 
 HYPHEN_E_DOT ='-e .'
 def get_requirements(file_path:str)->List[str]:
@@ -12,6 +12,7 @@ def get_requirements(file_path:str)->List[str]:
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
         requirements=[req.replace("/n","") for req in requirements]
+        print("just to test the code so far")
 
         if HYPHEN_E_DOT in requirements:
             requirements.remove(HYPHEN_E_DOT)
@@ -21,9 +22,9 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name='newMLProject',
     version='0.0.1',
-    author='Arpit',
+    author='Arpit Sinha',
     author_email='arpit.sinha1609@gmail.com',
     packages=find_packages(),
-    install_requires=['pandas','numpy','seaborn']
-    #install_requires=get_requirements('requirements.txt')
+    #install_requires=['pandas','numpy','seaborn']
+    install_requires=get_requirements('requirements.txt')
 )
